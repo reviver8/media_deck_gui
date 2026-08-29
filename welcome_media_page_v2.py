b03df8b8bc25927e6bfa86b67bc20c461179e797
reviver8 <trinitydcodes@gmail.com>
2026-08-29 02:15:25 -0400

    button icons and recent media layout: done: finished the layout for the button sections and the Recent Media section, the icons for the buttons have been placed to-do: polish the alignment of the button icons (no obvious alignment errors), implement the Notion API

--- welcome_media_page_v2.py
+++ welcome_media_page_v2.py
@@ -5,7 +5,7 @@ def do_nothing():
     return 0
 
 app = App(title="My app", width=960, height=540,layout="grid")
-app.bg = "#7E7E7E"
+app.bg = "#3F3F3F"
 
 top_pad = Box(app, grid=[0,0], height=20, width="fill")
 left_pad = Box(app, grid=[0,0], height="fill", width=15)
@@ -28,42 +28,46 @@ top_middle_pad = Box(app, grid=[2,1], height="fill", width=30)
 
 # Recent Media Display section
 recent_media_display_box = Box(app, layout="auto", grid=[3,1,5,1], height=216, width=app.width * 0.58)
+recent_media_display_box.bg = "#F15BB5"
 recent_media_display_box_pad = Box(recent_media_display_box, layout="auto", height=recent_media_display_box.height * 0.10, width="fill")
 recent_media_display_text = Text(recent_media_display_box, text="Recent Media", bold=True, size=32)
-recent_media_movie_text = Text(recent_media_display_box, text="Movie: None", size=18)
-recent_movie_pad = Box(recent_media_display_box, layout="auto", height=recent_media_display_box.height * 0.10, width="fill")
-recent_media_tv_text = Text(recent_media_display_box, text="TV Show: None", size=18)
-recent_tv_pad = Box(recent_media_display_box, layout="auto", height=recent_media_display_box.height * 0.10, width="fill")
-recent_media_book_text = Text(recent_media_display_box, text="Book: None", size=18)
-recent_book_pad = Box(recent_media_display_box, layout="auto", height=recent_media_display_box.height * 0.10, width="fill")
-recent_media_display_box.bg = "#F15BB5"
+
+recent_media_display_text_box = Box(recent_media_display_box, layout="grid", height=recent_media_display_box.height * 0.90, width="fill")
+recent_media_top_pad = Box(recent_media_display_text_box, grid=[0,0], height=recent_media_display_text_box.height * 0.22, width="fill")
+recent_media_left_pad = Box(recent_media_display_text_box, grid=[0,1], height="fill", width=40)
+
+recent_media_movie_text = Text(recent_media_display_text_box, grid=[1,1,1,2], text="Movie: None", size=18)
+recent_media_middle_left_pad = Box(recent_media_display_text_box, grid=[2,1], height="fill", width=20)
+recent_media_tv_text = Text(recent_media_display_text_box, grid=[3,1,1,2], text="TV Show: None", size=18)
+recent_media_middle_right_pad = Box(recent_media_display_text_box, grid=[4,1], height="fill", width=20)
+recent_media_book_text = Text(recent_media_display_text_box, grid=[5,1,1,2], text="Book: None", size=18)
+recent_media_right_pad = Box(recent_media_display_text_box, grid=[6,1], height="fill", width=40)
+
 
 top_right_pad = Box(app, grid=[9,1], height="fill", width=25)
 middle_pad = Box(app, grid=[0,2], height=20, width="fill")
 
 # Gif section
 gif_box = Box(app, layout="auto", grid=[1,3], height=app.height * 0.5, width=app.width * 0.35)
-gif_image = Picture(gif_box, image="assets\hi-res-shrek.gif", height=int(gif_box.height), width=int(gif_box.width))
+gif_image = Picture(gif_box, image="assets/shrek_smirk.gif", height=int(gif_box.height), width=int(gif_box.width))
 
 # Movie button section
 bottom_middle_left_pad = Box(app, grid=[2,3], height="fill", width=10)
 movie_button_box = Box(app, layout="auto", grid=[3,3], height=gif_image.height, width=app.width * 0.25)
-movie_button = PushButton(movie_button_box, text="add movie", command=do_nothing, height=100, width=100)
+movie_button = PushButton(movie_button_box, image="assets\movie_icon.png", command=do_nothing, height=gif_image.height, width=int(app.width * 0.25))
 movie_button_box.bg = "#9B5DE5"
 
-# TV and Book buttons section
-tv_book_buttons_height, tv_book_buttons_width = 40, 20
 
 # TV button section
 bottom_middle_middle_pad = Box(app, layout="auto", grid=[4,3], height="fill", width=10)
 tv_button_box = Box(app, layout="auto", grid=[5,3], height=gif_image.height, width=app.width * 0.15)
-tv_button = PushButton(tv_button_box, command=do_nothing, text="add TV show", height=tv_book_buttons_height, width=tv_book_buttons_width)
+tv_button = PushButton(tv_button_box, image="assets/tv_icon.png", command=do_nothing, height=int(gif_image.height), width= int(app.width * 0.15))
 tv_button_box.bg = "#00BBF9"
 
 # Book button section
 bottom_middle_right_pad = Box(app, layout="auto", grid=[6,3], height="fill", width=10)
-book_button_box = Box(app, layout="auto", grid=[7,3], height=gif_image.height, width=app.width * 0.15)
-book_button = PushButton(book_button_box, command=do_nothing, text="add book", height=tv_book_buttons_height, width=tv_book_buttons_width)
+book_button_box = Box(app, layout="auto", grid=[7,3], height=int(gif_image.height), width=app.width * 0.15)
+book_button = PushButton(book_button_box, image=r"assets\book_icon.png", command=do_nothing, height=gif_image.height, width= int(app.width * 0.15))
 book_button_box.bg = "#FEE440"
 
 update_clock()
